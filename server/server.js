0fbe61da511d4425bddab22768475c24184f8751
Duvini Nimethra <desilva-se22023@stu.kln.ac.lk>
2025-11-15 08:12:29 +0530

    Change server listen address to '0.0.0.0'

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`NutriLanka API ready on port ${PORT}`);
     });
   } catch (error) {
